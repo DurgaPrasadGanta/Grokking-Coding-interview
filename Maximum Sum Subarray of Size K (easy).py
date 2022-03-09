d7b77b28a19f8471522a9f7c5690d210b8ddb71d
DurgaPrasadGanta <56169161+DurgaPrasadGanta@users.noreply.github.com>
2022-03-09 09:32:20 -0600

    Update Maximum Sum Subarray of Size K (easy).py

--- Maximum Sum Subarray of Size K (easy).py
+++ Maximum Sum Subarray of Size K (easy).py
@@ -2,12 +2,12 @@
 Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
 
 Example: 1
-nums=[2, 3, 4, 1, 5]
-k=2
+nums=[2, 3, 4, 1, 5], k=2
+output: 7
 
 Example:2
-nums=[2, 1, 5, 1, 3, 2]
-k=3
+nums=[2, 1, 5, 1, 3, 2], k=3
+output: 9
 '''
 
 def maxSubarrayOfSizeK(nums,k):
