fd8d830b925c79c503e911dbe1683e443905e927
DurgaPrasadGanta <56169161+DurgaPrasadGanta@users.noreply.github.com>
2022-06-17 16:12:32 -0500

    Update and rename Maximum Sum Subarray of size k.py to Maximum Average Subarray of size k.py

--- Pattern Sliding Window/Maximum Average Subarray of size k.py
+++ Pattern Sliding Window/Maximum Average Subarray of size k.py
@@ -4,11 +4,11 @@ You are given an integer array nums consisting of n elements, and an integer k.
 
 Example 1:
 Input: [2, 1, 5, 1, 3, 2], k=3 
-Output: 9
+Output: 3
 Explanation: Subarray with maximum sum is [5, 1, 3].
 Example 2:
 Input: [2, 3, 4, 1, 5], k=2 
-Output: 7
+Output: 3.5
 Explanation: Subarray with maximum sum is [3, 4].
 '''
 
